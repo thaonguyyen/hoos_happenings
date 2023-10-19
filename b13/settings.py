dfaeb0d38eee213d133e42f6baa3f232073aa533
shaluram71 <shaluram71@gmail.com>
2023-10-19 16:02:53 -0400

    directory changes to accomodate Heroku

--- b13/settings.py
+++ b13/settings.py
@@ -65,7 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware'
+    # 'allauth.account.middleware.AccountMiddleware'
 ]
 
 ROOT_URLCONF = 'b13.urls'
