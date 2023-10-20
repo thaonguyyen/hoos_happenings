36f42aca842540f7541e69dc6a81397179131679
shaluram71 <shaluram71@gmail.com>
2023-10-20 00:42:08 -0400

    adding bootstrap to settings.py

--- b13/settings.py
+++ b13/settings.py
@@ -67,6 +67,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'bootstrap5',
 ]
 
 ROOT_URLCONF = 'b13.urls'
