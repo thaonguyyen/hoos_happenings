1f263adf125e83fb44675344b61f55354f24e3a0
thaonguyyen <vdk4dy@virginia.edu>
2023-11-02 21:53:59 -0400

    implemented most of admin approval. bugs with url patterns left to fix

--- b13/hoos/urls.py
+++ b13/hoos/urls.py
@@ -4,5 +4,21 @@ from . import views
 urlpatterns = [
     path("", views.home, name='home'),
     path("logout", views.logout_view, name='logout'),
-    path("map/", views.map, name='map'),
+    path("hoos_login/", views.admin_login, name='authenticate'),
+
+    path("hoos_admin/", views.admin_view, name='home_admin'),
+    path("hoos_admin/logout", views.logout_view, name='admin_logout'),
+    path("hoos_admin/map", views.admin_map, name='admin_map'),
+    path("hoos_admin/admin_home", views.admin_view, name='admin_home'),
+    path("hoos_admin/admin_map", views.admin_map, name='admin_map'),
+    path("hoos_admin/review_event/", views.review_events, name='review_event'),
+    path('hoos_admin/approve_event/<int:event_id>/', views.approve_event, name='approve_event'),
+    path('hoos_admin/admin_event_listings/', views.admin_event_listings, name='admin_event_listings'),
+
+    path("hoos_user/", views.user_view, name='home_user'),
+    path("hoos_user/submit_event", views.submit_event, name='submit_event'),
+    path("hoos_user/logout", views.logout_view, name='user_logout'),
+    path("hoos_user/user_home", views.user_view, name='user_home'),
+    path("hoos_user/user_map", views.user_map, name='user_map'),
+    path('hoos_user/user_event_listings/', views.user_event_listings, name='user_event_listings'),
 ]
--- b13/hoos/views.py
+++ b13/hoos/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from .forms import EventSubmissionForm
+from .models import EventSubmission
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def home(request):
@@ -9,6 +13,52 @@ def logout_view(request):
     logout(request)
     return redirect("/")
 
-def map(request):
-    return render(request, "map.html")
+def user_map(request):
+    return render(request, "user_map.html")
 
+def admin_map(request):
+    return render(request, "admin_map.html")
+
+def admin_login(request):
+    if request.user.is_authenticated and request.user.is_superuser:
+            return redirect('home_admin')
+    return redirect('home_user')
+
+def is_admin(user):
+    return user.is_superuser
+
+def admin_view(request):
+    return render(request, 'home_admin.html')
+
+def user_view(request):
+    return render(request, 'home_user.html')
+
+def submit_event(request):
+    if request.method == 'POST':
+        form = EventSubmissionForm(request.POST)
+        if form.is_valid():
+            submission = form.save(commit=False)
+            submission.user = request.user
+            form.save()
+            return redirect('submit_event')  # Redirect to a success page or wherever appropriate
+    else:
+        form = EventSubmissionForm()
+    return render(request, 'submit.html', {'form': form})
+
+def review_events(request):
+    events = EventSubmission.objects.filter(approved=False)
+    return render(request, 'review.html', {'events': events})
+
+def approve_event(request, event_id):
+    event = EventSubmission.objects.get(id=event_id)
+    event.approved = True
+    event.save()
+    return redirect('review_event')
+
+def user_event_listings(request):
+    events = EventSubmission.objects.filter(approved=True)
+    return render(request, 'user_listings.html', {'events': events})
+
+def admin_event_listings(request):
+    events = EventSubmission.objects.filter(approved=True)
+    return render(request, 'admin_listings.html', {'events': events})
