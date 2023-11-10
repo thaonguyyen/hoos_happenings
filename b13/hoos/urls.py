0992fae07225aee634361fc623822fe7eeebd8e2
jomoss25 <124541796+jomoss25@users.noreply.github.com>
2023-11-10 15:46:57 -0500

    Re-adding files

---
+++ b13/hoos/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name='home'),
+    path("logout", views.logout_view, name='logout'),
+    path("map/", views.map, name='map'),
+]
