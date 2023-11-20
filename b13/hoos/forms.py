37a56f8cbc7f9ce8c552f77837a38efa40947f2d
jomoss25 <124541796+jomoss25@users.noreply.github.com>
2023-11-20 17:08:57 -0500

    Added tags to events

--- b13/hoos/forms.py
+++ b13/hoos/forms.py
@@ -4,4 +4,4 @@ from .models import EventSubmission
 class EventSubmissionForm(forms.ModelForm):
     class Meta:
         model = EventSubmission
-        fields = ['name', 'description', 'location']
+        fields = ['name', 'description', 'location', 'tag']
--- b13/hoos/models.py
+++ b13/hoos/models.py
@@ -1,7 +1,8 @@
-from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 class EventSubmission(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -11,6 +12,23 @@ class EventSubmission(models.Model):
     latitude = models.FloatField(null=True, blank=True)
     longitude = models.FloatField(null=True, blank=True)
     approved = models.BooleanField(default=False)
+    class Tags(models.TextChoices):
+        CAREER = "career", _("Career")
+        CLUB = "club", _("Club")
+        COLLEGE = "arts & sciences", _("Arts and Sciences")
+        CONCERT = "concert", _("Concert")
+        CULTURE = "culture", _("Culture")
+        EDU = "education", _("Education")
+        ENG = "engineering", _("Engineering")
+        FOOD = "food", _("Food")
+        MCINTIRE = "mcintire", _("McIntire")
+        RELIGIOUS = "religious", _("Religious")
+        SOCIAL = "social", _("Social")
+        SPORTS = "sports", _("Sports")
+        THEATRE = "theatre", _("Theatre")
+        __empty__ = _("No Tags")
+
+    tag = models.CharField(max_length=200, choices=Tags.choices, null=True)
 
     def __str__(self):
         return f"{self.name} by {self.user.username}"
