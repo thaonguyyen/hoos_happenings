2f32687029fa6537ebd415c2fd52274ea4c4cc6d
Peter <fpv5gr@virginia.edu>
2023-11-24 11:23:46 -0500

    clean up models.py

--- b13/hoos/models.py
+++ b13/hoos/models.py
@@ -1,8 +1,6 @@
 from django.conf import settings
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from django.contrib.postgres.fields import ArrayField
 
 
 class Tag(models.Model):
@@ -10,24 +8,6 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
-
-
-# class Tags(models.TextChoices):
-#     CAREER = "career", _("Career")
-#     CLUB = "club", _("Club")
-#     COLLEGE = "arts & sciences", _("Arts and Sciences")
-#     CONCERT = "concert", _("Concert")
-#     CULTURE = "culture", _("Culture")
-#     EDU = "education", _("Education")
-#     ENG = "engineering", _("Engineering")
-#     FOOD = "food", _("Food")
-#     MCINTIRE = "mcintire", _("McIntire")
-#     RELIGIOUS = "religious", _("Religious")
-#     SOCIAL = "social", _("Social")
-#     SPORTS = "sports", _("Sports")
-#     THEATRE = "theatre", _("Theatre")
-#     __empty__ = _("No Tags")
-
 
 class EventSubmission(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
