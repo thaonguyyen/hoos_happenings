7f9537c766134dfd6201163f40d993a2a6fe45bf
RowTheBoat <rowenrlink@gmail.com>
2023-11-30 15:31:36 -0500

    Fixing operational error bug

--- b13/b13/settings.py
+++ b13/b13/settings.py
@@ -104,7 +104,7 @@ if IS_HEROKU_APP:
 
     DATABASES = {
         "default": dj_database_url.config(
-            conn_max_age=600,
+            conn_max_age=0,
             conn_health_checks=True,
             ssl_require=True,
         ),
